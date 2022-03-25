a48919951fdd0cefcf337f3353b0550aa328d7bf
Justin <kaytin143@gmail.com>
2022-03-24 22:06:11 -0700

    added sidebar

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import './style.css'
 var main = document.getElementById('main')
 var header = document.createElement('div')
 var headerTitle = document.createElement('h1')
+var sideBar = document.createElement('div')
+sideBar.classList.add('sideBar-container')
 header.classList.add('header')
 headerTitle.textContent = 'To-Do List ✅'
-main.append(header)
-header.appendChild(headerTitle)
 
+
+
+main.append(header)
+main.append(sideBar)
+header.appendChild(headerTitle)
