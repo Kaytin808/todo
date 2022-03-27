61daf1ffcda21c29bdb55d23b3be14eab62b8975
Justin Moore <kaytin143@gmail.com>
2022-03-27 14:26:10 -0700

    Added Inbox section and Add task to page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import './style.css'
+
+
+
 var main = document.getElementById('main')
 var header = document.createElement('div')
 var headerTitle = document.createElement('h1')
 var sideBar = document.createElement('div')
+var middleBar = document.createElement('div')
+var container = document.createElement('div')
+var inbox = document.createElement('div')
+var inboxP = document.createElement('p')
+var addTask = document.createElement('div')
+var spanAdd = document.createElement('div')
+spanAdd.classList.add('add')
+spanAdd.textContent = '+'
+var addTaskP = document.createElement('span')
+addTaskP.textContent = 'Add Task'
+addTask.classList.add('add-task')
+var taskContainer = document.createElement('div')
+taskContainer.classList.add('task-container')
+inbox.classList.add('inbox')
+container.classList.add('container')
+middleBar.classList.add('middle-container')
 sideBar.classList.add('sideBar-container')
 header.classList.add('header')
 headerTitle.textContent = 'To-Do List ✅'
-
+inboxP.textContent = 'Inbox'
 
 
 main.append(header)
-main.append(sideBar)
+main.append(container)
+container.append(sideBar)
+sideBar.appendChild(inbox)
+inbox.appendChild(inboxP)
+container.append(middleBar)
+middleBar.appendChild(taskContainer)
+taskContainer.appendChild(addTask)
+addTask.appendChild(spanAdd)
+spanAdd.appendChild(addTaskP)
 header.appendChild(headerTitle)
