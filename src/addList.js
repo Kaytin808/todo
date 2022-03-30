8a7751f909b7ec3e739f76dc805a288df1509fe2
Justin <kaytin143@gmail.com>
2022-03-30 07:30:03 -0700

    Added remove button

--- src/addList.js
+++ src/addList.js
@@ -3,21 +3,34 @@ export default function addList() {
         title:document.getElementById('task').value,
         description:document.getElementById('description').value
     }
-    if (todoInfo.title == '') {
-        alert(' u entered nothin')
-    } else {
+
         addThis();
-    }
+    
 
 function addThis() {
     var h3 = document.createElement('h3')
     var li = document.createElement('li')
     var ul = document.querySelector('.ul-list')
     var div = document.createElement('div')
+    var x = document.createElement('div')
+    x.classList.add('delete-task')
+    div.classList.add('list-border')
+
     h3.textContent = `${todoInfo.title}`
-    li.textContent = `${todoInfo.description}`
+    x.textContent ='✖'
+    li.textContent = `${todoInfo.description} `
+    li.style.textAlign = 'center'
+    h3.style.textAlign ='center'
+        div.addEventListener('click', ()=>{
+            div.style.textDecoration = 'line-through'
+    })
+    x.addEventListener('click', () => {
+        div.remove();
+    })
     ul.appendChild(div)
+    div.append(x)
     div.appendChild(h3)
     div.appendChild(li)
+    
     }
 }
