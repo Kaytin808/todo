6d3c9047c552bdc37ee512b2207ed23c720920a0
Justin Moore <kaytin143@gmail.com>
2022-05-09 12:28:08 -0700

    Changed html skelly

---
+++ src/inboxListCreate.js
@@ -0,0 +1,48 @@
+export default function createList() {
+    var titleInbox = document.getElementById('title-inbox')
+    var descInbox = document.getElementById('desc-inbox')
+    let todoInfo = {
+        title:document.getElementById('title-inbox').value,
+        description:document.getElementById('desc-inbox').value,
+        date:document.getElementById('date-inbox').value
+    }
+
+        document.getElementById('title-inbox').value = ''
+        document.getElementById('desc-inbox').value = ''
+
+
+    
+            var h3 = document.createElement('h3')
+            var li = document.createElement('li')
+            var mkDate = document.createElement('p')
+            var div = document.createElement('div')
+            var x = document.createElement('div')
+            var ul_inbox = document.querySelector('.ul-list-inbox')
+            x.classList.add('delete-task')
+            div.classList.add('list-border')
+
+            h3.textContent = `${todoInfo.title}`
+            x.textContent ='✖'
+            li.textContent = `${todoInfo.description} `
+            mkDate.textContent = `${todoInfo.date}`
+            li.style.textAlign = 'center'
+            h3.style.textAlign ='center'
+                div.addEventListener('click', ()=>{
+                    div.style.textDecoration = 'line-through'
+            })
+            x.addEventListener('click', () => {
+                div.remove();
+            })
+
+            ul_inbox.appendChild(div)
+            div.append(x)
+            div.appendChild(h3)
+            div.appendChild(li)
+            li.appendChild(mkDate)
+            document.getElementById('title-inbox').value = ''
+            document.getElementById('desc-inbox').value = ''
+            h3.textContent = `${todoInfo.title}`
+            if (`${todoInfo.title}`== '') {
+            h3.remove()
+            }
+        }
--- src/index.js
+++ src/index.js
@@ -1,193 +1,110 @@
-// import addList from './addList';
+// import createInboxContent from './inboxContent'
 import './style.css'
-
+import createList from './inboxListCreate'
+import createRanList from '../createProject'
 var main = document.getElementById('main')
-var header = document.createElement('div')
-var headerTitle = document.createElement('h1')
-var sideBar = document.createElement('div')
-var middleBar = document.createElement('div')
-var container = document.createElement('div')
-var inbox = document.createElement('div')
-var inboxP = document.createElement('p')
-var today = document.createElement('div')
-var todayP = document.createElement('p')
-var project = document.createElement('div')
-var projectP = document.createElement('p')
-var listContainerInbox = document.createElement('div')
-var listContainerProject = document.createElement('div')
-var listContainerToday = document.createElement('div')
+const subnav = document.querySelector('.subnav')
+const middleBar = document.querySelector('.middle-container');
+// var inbox = document.querySelector('.inbox')
+// var today = document.querySelector('.today')
+// var project = document.querySelector('.project')
+// var middleBar = document.querySelector('.middle-container')
+// var listContainerProject = document.createElement('div')
+// var listContainerToday = document.createElement('div')
 
+function setupTabs() {
+    document.querySelectorAll(".tabs_buttons").forEach(button => {
+        button.addEventListener('click', () => {
+            const sideBar = button.parentElement;
+            const tabsContainer = document.querySelector('.middle-container');
+            const tabNumber = button.dataset.forTab;
+            const tabToActivate = tabsContainer.querySelector(`.tabs_content[data-tab="${tabNumber}"]`)
 
-
-
-// class-add //
-
-
-listContainerInbox.classList.add('hidden')
-listContainerProject.classList.add('hidden')
-listContainerToday.classList.add('hidden')
-inbox.classList.add('inbox')
-today.classList.add('today')
-project.classList.add('project')
-container.classList.add('container')
-middleBar.classList.add('middle-container')
-sideBar.classList.add('sideBar-container')
-header.classList.add('header')
-
-// text-content //
-headerTitle.textContent = 'To-Do List ✅'
-inboxP.textContent = 'Inbox'
-todayP.textContent = 'Today'
-projectP.textContent = 'Projects'
-
-function createInboxContent() {
-    var ul_inbox = document.createElement('ul')
-    var div = document.createElement('div')
-    var input1 = document.createElement('input')
-    var div1 = document.createElement('div')
-    var div2 = document.createElement('div')
-    var input2 = document.createElement('input')
-    var submitB = document.createElement('button')
-    var h3 = document.createElement('h3')
-    var dateInput = document.createElement('input')
-    var today = new Date();
-    var dateNow = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getFullYear(); 
-    dateInput.type = 'date'
-    dateInput.id = 'date'
-    dateInput.setAttribute('value', dateNow)
-    // dateInput.setAttribute('min', today)
-    // dateInput.setAttribute('max', '2023-04-16')
-    h3.classList.add('inboxTitle')
-    var divContainer = document.createElement('form')
-    input1.setAttribute('name', 'title')
-    input1.id = 'title-inbox'
-    input1.setAttribute('type','text')
-    input1.setAttribute('placeholder','Title here')
-    input2.setAttribute('name', 'desc')
-    input2.setAttribute('type','text')
-    input2.setAttribute('placeholder','Desc here')
-    input2.id = 'desc-inbox'
-    h3.textContent = 'Inbox'
-    ul_inbox.classList.add('ul-list-inbox')
-    divContainer.classList.add('form-container')
-    submitB.setAttribute('type','button')
-    submitB.setAttribute('value','Submit')
-    submitB.textContent = '+'
-    submitB.addEventListener('click', () => {
-        if (input1.value.length == 20 || input2.value.length == 0) {
-            alert('Description cannot be empty!')
-        } else {
-            let todoInfo = {
-                title:document.getElementById('title-inbox').value,
-                description:document.getElementById('desc-inbox').value,
-                date:document.getElementById('date').value
-            }
-
-            var h3 = document.createElement('h3')
-            var li = document.createElement('li')
-            var mkDate = document.createElement('p')
-            var ul = document.querySelector('.ul-list-inbox')
-            var div = document.createElement('div')
-            var x = document.createElement('div')
-            x.classList.add('delete-task')
-            div.classList.add('list-border')
-        
-            h3.textContent = `${todoInfo.title}`
-            if (`${todoInfo.title}`== '') {
-        
-            }
-            x.textContent ='✖'
-            li.textContent = `${todoInfo.description} `
-            mkDate.textContent = `${todoInfo.date}`
-            li.style.textAlign = 'center'
-            h3.style.textAlign ='center'
-                div.addEventListener('click', ()=>{
-                    div.style.textDecoration = 'line-through'
+            sideBar.querySelectorAll('.tabs_buttons').forEach(button => {
+                button.classList.remove("tabs_buttons--active")
             })
-            x.addEventListener('click', () => {
-                div.remove();
+
+            tabsContainer.querySelectorAll('.tabs_content').forEach(tab => {
+                tab.classList.remove("tabs_content--active")
             })
-           
-            ul.appendChild(div)
-            div.append(x)
-            div.appendChild(h3)
-            div.appendChild(li)
-            li.appendChild(mkDate)
-            document.getElementById('title-inbox').value = ''
-            document.getElementById('desc-inbox').value = ''
-            h3.textContent = `${todoInfo.title}`
-            if (`${todoInfo.title}`== '') {
-        h3.remove()
-            }
-        }
+
+            button.classList.add('tabs_button--active');
+            tabToActivate.classList.add('tabs_content--active')
+        })
     })
-    listContainerInbox.appendChild(h3)
-    listContainerInbox.appendChild(ul_inbox)
-    listContainerInbox.appendChild(divContainer)
-    // listContainerInbox.appendChild(h3)
-    divContainer.appendChild(div)
-    div.appendChild(input1)
-    divContainer.appendChild(div1)
-    divContainer.appendChild(div2)
-    div1.appendChild(input2)
-    div2.appendChild(dateInput)
-    divContainer.appendChild(submitB)
-    return ul_inbox;
 }
+setupTabs();
 
+var inboxListen = document.querySelector('.submit-inbox-button')
+inboxListen.addEventListener('click', () => {
+    createList();
+})
+
+// var createProjectListener = document.querySelector('.create-project')
+// createProjectListener.addEventListener('click', () => {
+//     createProjectContent()
+// })
+// var defaultProjectListen = document.querySelector('.default-project')
+// defaultProjectListen.addEventListener('click', () => {
+
+// })
+const plus = document.querySelector('.plus')
+plus.addEventListener('click', () => {
+    createProjectContent();
+})
 function createProjectContent() {
 let projectName = prompt('What is the name of your project?')
-var h3 = document.createElement('h3')
-var newTab = document.createElement('div')
-// var newTabP = document.createElement('div')
-// newTabP.textContent = projectName
+var newTab = document.createElement('button')
+const prDisplay = document.querySelector('.create-project')
+var title = document.createElement('h3')
+var tabs_content = document.createElement('div')
+tabs_content.classList.add('tabs_content')
+tabs_content.dataset.tab = projectName;
+newTab.classList.add('tabs_buttons')
+newTab.dataset.forTab = projectName;
 newTab.textContent = projectName;
-// h3.textContent = projectName;
-sideBar.appendChild(newTab)
-// newTab.appendChild(newTabP)
-}
-function createTodayContent() {
-    var todayTitle = document.createElement('h3')
-    todayTitle.textContent = 'Today'
-    todayTitle.classList.add('today-title')
-    listContainerToday.appendChild(todayTitle)
-    return todayTitle
-}
-// event Listeners //
+title.textContent = projectName;
+title.classList.add('titleMiddleBar')
 
-project.addEventListener('click', () => {
-listContainerProject.className = 'list-container-project'
-listContainerInbox.className = 'hidden'
-listContainerToday.className = 'hidden'
-createProjectContent()
-})
+subnav.appendChild(newTab)
+middleBar.appendChild(tabs_content)
+tabs_content.appendChild(title)
+createRanList();
+setupTabs();
 
-inbox.addEventListener('click', () => {
-    listContainerProject.className = 'hidden'
-    listContainerInbox.className ='list-container-inbox'
-    listContainerToday.className = 'hidden'
-})
+} 
+// function createTodayContent() {
+//     var todayTitle = document.createElement('h3')
+//     todayTitle.textContent = 'Today'
+//     todayTitle.classList.add('today-title')
+//     listContainerToday.appendChild(todayTitle)
+//     return todayTitle
+// }
+// // event Listeners //
 
-today.addEventListener('click', () => {
-    listContainerInbox.className = 'hidden'
-    listContainerProject.className = 'hidden'
-    listContainerToday.className = 'list-container-today'
-})
+// project.addEventListener('click', () => {
+// listContainerProject.className = 'list-container-project'
+// listContainerInbox.className = 'hidden'
+// listContainerToday.className = 'hidden'
+// // createProjectContent()
+// })
 
-main.append(header)
-main.append(container)
-container.append(sideBar)
-sideBar.appendChild(inbox)
-inbox.appendChild(inboxP)
-sideBar.appendChild(today)
-today.appendChild(todayP)
-sideBar.appendChild(project)
-project.appendChild(projectP)
-container.append(middleBar)
-middleBar.append(listContainerInbox)
-middleBar.appendChild(listContainerProject)
-middleBar.appendChild(listContainerToday)
-// listContainerProject.appendChild(createProjectContent())
-listContainerInbox.appendChild(createInboxContent())
-listContainerToday.appendChild(createTodayContent())
-header.appendChild(headerTitle)
+// inbox.addEventListener('click', () => {
+//     listContainerProject.className = 'hidden'
+//     listContainerInbox.className ='list-container-inbox'
+//     listContainerToday.className = 'hidden'
+// })
+
+// today.addEventListener('click', () => {
+//     listContainerInbox.className = 'hidden'
+//     listContainerProject.className = 'hidden'
+//     listContainerToday.className = 'list-container-today'
+// })
+
+
+// middleBar.appendChild(createInboxContent())
+// middleBar.appendChild(listContainerProject)
+// middleBar.appendChild(listContainerToday)
+// // listContainerProject.appendChild(createProjectContent())
+// // listContainerInbox.appendChild(createInboxContent())
+// listContainerToday.appendChild(createTodayContent())
