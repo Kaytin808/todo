a57d8c94de428eec648be846c35460038c68addc
Justin Moore <kaytin143@gmail.com>
2022-06-23 00:47:46 -0700

    Removed title and only added desc and date field

--- src/index.js
+++ src/index.js
@@ -45,50 +45,104 @@ function createProjectContent() {
   var newTab = document.createElement("button");
   var title = document.createElement("h3");
   var tabs_content = document.createElement("div");
-  var ulTest = document.createElement("ul");
-  ulTest.classList.add(".testMe");
+  var ul = document.createElement("ul");
+  ul.classList.add("ul-form");
   tabs_content.classList.add("tabs_content");
   tabs_content.dataset.tab = projectName;
   newTab.classList.add("tabs_buttons");
   newTab.dataset.forTab = projectName;
   newTab.textContent = projectName;
-  title.textContent = projectName;
+  title.textContent = projectName.toUpperCase();
   title.classList.add("titleMiddleBar");
 
   var formContainer = document.createElement("div");
   formContainer.classList.add("form-container");
   var ulList = document.createElement("ul");
   ulList.classList.add("ul-list");
-  var titleDiv = document.createElement("div");
+  // var titleDiv = document.createElement("div");
   var descDiv = document.createElement("div");
   var dateDiv = document.createElement("div");
 
-  var titleInput = document.createElement("input");
-  titleInput.setAttribute("name", "title");
-  titleInput.id = "title-inbox";
-  titleInput.setAttribute("type", "text");
-  titleInput.setAttribute("placeholder", "Title here...");
+  // var titleInput = document.createElement("input");
+  // titleInput.setAttribute("name", "title");
+  // titleInput.id = "title-form";
+  // titleInput.setAttribute("type", "text");
+  // titleInput.setAttribute("placeholder", "Title here...");
 
   var descInput = document.createElement("input");
   descInput.setAttribute("name", "description");
-  descInput.id = "desc-inbox";
+  descInput.id = "desc-form";
   descInput.setAttribute("type", "text");
-  descInput.setAttribute("placeholder", "description");
+  descInput.setAttribute("placeholder", "Description");
 
   var dateInput = document.createElement("input");
-  dateInput.setAttribute("id", "date-inbox");
-  dateInput.setAttribute("type", "date");
+  dateInput.setAttribute("id", "date-form");
+  dateInput.setAttribute("type", "text");
+  dateInput.setAttribute("placeholder", "Date here~")
+  dateInput.setAttribute("onfocus", "this.type='date'")
+  dateInput.setAttribute("onblur", "this.type='text'")
 
   var submitButton = document.createElement("button");
   submitButton.setAttribute("type", "submit");
   submitButton.setAttribute("value", "Submit");
   submitButton.classList.add("formSubmitBtn");
   submitButton.innerText = "+";
+  submitButton.addEventListener('click', () => {
+    
+    let todoInfo = {
+      // title: document.getElementById("title-form").value,
+      description: document.getElementById("desc-form").value,
+      date: document.getElementById("date-form").value,
+    }
+    if (`${todoInfo.description}` == "" && `${todoInfo.date}` == "") {
+      alert("Can't be empty!")
+      return;
+    } else if (todoInfo.date == "") {
+      alert('Needs a date!')
+      return;
+    }
+    // document.getElementById("title-inbox").value = "";
+    // document.getElementById("desc-inbox").value = "";
+  
+    // var h3 = document.createElement("h3");
+    var li = document.createElement("li");
+    var mkDate = document.createElement("p");
+    var div = document.createElement("div");
+    var x = document.createElement("div");
+    // var ul_inbox = document.querySelector(".ul-list-inbox");
+    x.classList.add("delete-task");
+    div.classList.add("list-border");
+  
+    // h3.textContent = todoInfo.title.toUpperCase();
+    x.textContent = "✖";
+    li.textContent = `${todoInfo.description} `;
+    mkDate.textContent = `${todoInfo.date}`;
+    li.style.textAlign = "center";
+    li.style.padding = '5px'
+    // h3.style.textAlign = "center";
+    div.addEventListener("click", () => {
+      div.style.textDecoration = "line-through";
+    });
+    x.addEventListener("click", () => {
+      div.remove();
+    });
+  
+    ul.appendChild(div);
+    div.append(x);
+    // div.appendChild(h3);
+    div.appendChild(li);
+    li.appendChild(mkDate);
+    // document.getElementById("title-form").value = "";
+    document.getElementById("desc-form").value = "";
+    // h3.textContent = `${todoInfo.title}`;
+  })
+  
 
   tabs_content.appendChild(title);
   tabs_content.appendChild(formContainer);
-  formContainer.appendChild(titleDiv);
-  titleDiv.appendChild(titleInput);
+  tabs_content.appendChild(ul)
+  // formContainer.appendChild(titleDiv);
+  // titleDiv.appendChild(titleInput);
   formContainer.appendChild(descDiv);
   descDiv.appendChild(descInput);
   formContainer.appendChild(dateDiv);
